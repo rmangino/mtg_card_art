0fc83875b7b0b9b8213d165e78fa4e118ba2000e
Reed Mangino <reed@themanginos.com>
2015-12-15 11:49:43 -0500

    Basic config/setup

--- lib/mtg_card_art.rb
+++ lib/mtg_card_art.rb
@@ -1,5 +1,14 @@
 require "mtg_card_art/version"
 
+begin
+  require "pry"
+rescue LoadError
+end
+
 module MtgCardArt
-  # Your code goes here...
+  # binding.pry # Used to easily debug right at startup
+  class Deckbox
+    def art_for_cardname(name)
+    end
+  end
 end
